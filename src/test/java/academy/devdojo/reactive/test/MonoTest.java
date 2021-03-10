d905e33d6274083d952d536e3274ba28ed69bd11
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2021-03-10 16:14:33 -0300

    :memo:  Quick recap reactive streams

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -4,6 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
+/**
+ * Reactive Streams: É apenas um padrão, onde temos apenas 4 interfaces.
+ *
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ *
+ * - Publisher: Vai emitir os eventos, quem não der o subscribe, ele não emite nada (COLD)
+ * - Subscriber: Quem faz um [subscribe] no publisher.
+ * - Subscription: Objeto criado quando um SUBSCRIBER faz um SUBSCRIBE.
+ *
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ *  1. Publisher sends all the objects requested.
+ *  2. Publisher sends all the objects it has. (onComplete) -> subscriber and subscription will be canceled.
+ *  3. There is an error. (onError) -> subscriber and subscription will be canceled.
+ */
 public class MonoTest {
 
     @Test
