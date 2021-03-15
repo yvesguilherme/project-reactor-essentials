0eb7bee2d701433bd2d091e0daaa6f6931a74c41
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2021-03-15 15:55:12 -0300

    :sparkles:  Backpressure on Flux publisher with BaseSubscriber

--- src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -80,7 +81,7 @@ public class FluxTest {
         fluxInteger.subscribe(new Subscriber<Integer>() {
             private int count = 0;
             private Subscription subscription;
-            private int requestCount = 2;
+            private final int requestCount = 2;
 
             @Override
             public void onSubscribe(Subscription subscription) {
@@ -106,6 +107,38 @@ public class FluxTest {
             @Override
             public void onComplete() {
 
+            }
+        });
+
+        log.info("-------------------------------------------");
+
+        StepVerifier.create(fluxInteger)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersNotSoUglyBackpressure() {
+        Flux<Integer> fluxInteger = Flux.range(1, 10)
+                .log();
+
+        fluxInteger.subscribe(new BaseSubscriber<>() {
+            private int count = 0;
+            private final int requestCount = 2;
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(requestCount);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                count++;
+
+                if (count >= requestCount) {
+                    count = 0;
+                    request(requestCount);
+                }
             }
         });
 
